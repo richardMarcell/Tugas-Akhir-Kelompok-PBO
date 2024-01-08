5dfd1db9198404c274a01ab46c66c8b8d5bc3e38
Clay Aiken <120661939+Clayy9@users.noreply.github.com>
2024-01-08 19:12:41 +0700

    add: comment in Customer.java

--- src/Customer.java
+++ src/Customer.java
@@ -9,16 +9,20 @@
  * @author ACER
  */
 class Customer {
+    // Atribut kelas Customer
     String id_customer;
     String nama;
     String no_hp;
 
+    // Metode Register, untuk penambahan data customer baru
     void register(String id_customer, String nama, String no_hp) {
+        // Nilai yang berasal dari parameter yaitu id_customer, nama, dan no_hp menjadi nilai dari atribut kelas
         this.id_customer = id_customer;
         this.nama = nama;
         this.no_hp = no_hp;
     }
 
+    // Menggunakan toString agar dapat merepresentasikan string dari objek Customer yaitu atribut kelas Customer
     @Override
     public String toString() {
         return "ID Customer: " + id_customer  + "\nNama: " + nama + "\nNo HP: " + no_hp + "\n";
