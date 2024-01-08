154b2e529807afe5065da770847f646e71eed87d
Clay Aiken <120661939+Clayy9@users.noreply.github.com>
2024-01-08 19:08:17 +0700

    add : comment in Admin.java

--- src/Admin.java
+++ src/Admin.java
@@ -1,8 +1,11 @@
 public class Admin {
+    // Atribut kelas Admin
     String username = "Admin";
     String password = "password123";
 
+    // Metode Login
     boolean login(String usernameInput, String passwordInput) {
+        // Equals digunakan untuk membandingkan nilai dari variabel username dan usernameInput apakah sama atau tidak, jika sama nilainya "true"
         return username.equals(usernameInput) && password.equals(passwordInput);
     }
 }
