b2d6a1561e2554ccb4867c6cef363abb580806ac
Clay Aiken <120661939+Clayy9@users.noreply.github.com>
2024-01-08 19:17:17 +0700

    add: comment in DetailTransaksiPenitipan.java

--- src/DetailTransaksiPenitipan.java
+++ src/DetailTransaksiPenitipan.java
@@ -8,19 +8,21 @@
  *
  * @author ACER
  */
-import java.time.Duration;
-import java.time.LocalDateTime;
+import java.time.Duration; // Mengimpor kelas Duration yang merupakan bagian dari paket java.time agar dapat menghitung selisih waktu
+import java.time.LocalDateTime; // Mengimpor kelas LocalDateTime yang merupakan bagian dari paket java.time agar dapat nilai waktu terkini saat code dijalankan
 
 class DetailTransaksiPenitipan {
+    // Atribut kelas DetailTransaksiPenitipan
     int durasi_penitipan;
     double tagihan;
 
+    // Metode hitungTagihan, untuk menghitung tagihan yang harus dibayarkan oleh customer
     void hitungTagihan(LocalDateTime waktu_masuk, LocalDateTime waktu_keluar) {
-        Duration selisih = Duration.between(waktu_masuk, waktu_keluar);
-        long selisihJam = selisih.toHours();
-        selisihJam = Math.max(1, selisihJam);
+        Duration selisih = Duration.between(waktu_masuk, waktu_keluar); // Menghitung selisih waktu masuk dan waktu keluar customer
+        long selisihJam = selisih.toHours(); // Karena LocalDateTime adalah data waktu yang lengkap dengan menit dan detik, maka di convert ke jam agar memudahkan perhitungan
+        selisihJam = Math.max(1, selisihJam); // Ada kemungkinan selisih waktu kurang dari 1 jam, maka dari itu disetting minimal nilainya adalah 1 jam
 
-        tagihan = selisihJam * 2000;
+        tagihan = selisihJam * 2000; // Harga penitipan adalah Rp2,000 per jam
 
         System.out.println("Total Tagihan : Rp" + tagihan);
     }
